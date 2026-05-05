eafc858c24a3d21a2c3d0216ec061cb6aaadaead
Benjamin Speedy <bnspeedy@gmail.com>
2026-05-05 10:26:35 +1000

    fix helix top bar to position: fixed for stable nav

--- src/components/HelixPortfolio.tsx
+++ src/components/HelixPortfolio.tsx
@@ -498,7 +498,7 @@ function TopBar({ isMobile }: { isMobile: boolean }) {
   return (
     <div
       style={{
-        position: "absolute",
+        position: "fixed",
         top: 0,
         left: 0,
         right: 0,
